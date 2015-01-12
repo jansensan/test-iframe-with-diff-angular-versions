fe7105f65d60fc9b49b0a0e2e3719ee7419982f4
Mat Janson Blanchet <mat.jb@jansensan.net>
2015-01-12 14:46:50 +0100

    Added a line to skip the requests from the static dir.

--- iframe-container/src/js/api/bp-status/bp-status-mock.js
+++ iframe-container/src/js/api/bp-status/bp-status-mock.js
@@ -9,6 +9,7 @@
     
   function BpStatusMock($httpBackend, BpStatusMockData, BpStatusConfig) {
     var URL = BpStatusConfig.BASE_URL;
+    $httpBackend.whenGET(/^static\//).passThrough();
     $httpBackend.whenGET(URL).respond(BpStatusMockData);
   }
 
