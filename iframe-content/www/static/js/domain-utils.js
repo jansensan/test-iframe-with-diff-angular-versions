eade7eb329635290f35338917a1da5eb3c946a45
Mat Janson Blanchet <mat.jb@jansensan.net>
2015-01-13 11:36:53 +0100

    Added undefined check for setting document.domain.

---
+++ iframe-content/www/static/js/domain-utils.js
@@ -0,0 +1,15 @@
+function setDomain() {
+  var domainParts = window.location.hostname.split('.'),
+    topLevelDomain = domainParts[domainParts.length - 1],
+    secondLevelDomain = domainParts[domainParts.length - 2];
+
+  if(secondLevelDomain !== undefined && topLevelDomain !== undefined) {
+    document.domain = domainParts[domainParts.length - 2] + '.' + domainParts[domainParts.length - 1];
+
+  } else {
+    console.warn('Could not set document.domain, one of the domain\'s parts may be undefined.');
+  }
+
+}
+
+setDomain();
